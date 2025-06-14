eba47495af4c6c5bd63a1e6b55b9629f5de2d12c
Andrey Kruglov <kruglov.it@gmail.com>
2025-06-14 03:04:36 +0300

    feat: ConstructorElement story

---
+++ src/stories/components/constructor-element.stories.tsx
@@ -0,0 +1,45 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+import { fn } from 'storybook/test';
+import {ConstructorElement} from "../../lib";
+import bun from "./images/img.png";
+
+const meta: Meta<typeof ConstructorElement> = {
+    title: "Components/ConstructorElement",
+    component: ConstructorElement,
+    args: { handleClose: fn() },
+} satisfies Meta<typeof ConstructorElement>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// <ConstructorElement type='top' isLocked={true} text='Краторная булка N-200i (верх)' price={200} thumbnail={img} />
+// <ConstructorElement text='Краторная булка N-200i (верх)' price={50} thumbnail={img} />
+// <ConstructorElement type='bottom' isLocked={true} text='Краторная булка N-200i (низ)' price={200} thumbnail={img} />
+
+export const Top: Story = {
+    args: {
+        type: 'top',
+        isLocked: true,
+        text: 'Краторная булка N-200i (верх)',
+        price: 200,
+        thumbnail: bun
+    }
+};
+
+export const Normal: Story = {
+    args: {
+        text: 'Краторная булка N-200i',
+        price: 50,
+        thumbnail: bun
+    }
+};
+
+export const Bottom: Story = {
+    args: {
+        type: 'bottom',
+        isLocked: true,
+        text: 'Краторная булка N-200i (низ)',
+        price: 200,
+        thumbnail: bun
+    }
+};
